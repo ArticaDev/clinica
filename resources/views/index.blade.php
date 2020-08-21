ce83f8e696b6d1d8dc9d7d16f6a127bfa15ed256
Lucas Lima <limalucas454@gmail.com>
2020-08-20 23:27:32 -0300

    Initial page setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,82 @@
 @extends('layouts.default')
 @section('content')
-    <p>Content</p>
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="#"><img src="assets/img/0cd22a36-c30f-4004-9cea-0752a4fc9cc5-removebg-preview.png" style="height: 50px;width: 100px;"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="catalog-page.html">Catalog</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="features.html">Features</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="pricing.html">Pricing</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="about-us.html">About Us</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <main class="page landing-page">
+        <section class="clean-block clean-hero" style="color: rgba(0,0,0,0.85);background: url(https://images.pexels.com/photos/4672626/pexels-photo-4672626.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=650&amp;w=940) center;height: 60vh;">
+            <div class="text">
+                <h1 style="font-family: 'Dancing Script', cursive;color: #efc585;">O que você deseja?</h1>
+                <p style="font-family: 'Dancing Script', cursive;font-size: 42px;">Rejuvenescer</p>
+            </div>
+        </section>
+        <section class="clean-block clean-info dark">
+            <div class="container">
+                <div class="block-heading">
+                    <h1 style="color: rgb(239,197,133);">Escolha por <strong>ÁREA</strong></h1>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-lg-4 col-xl-3" style="margin: 10px auto;"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg" style="border-radius: 100%;width: 250px;height: 250px;"></div>
+                    <div class="col-md-6 col-lg-4 col-xl-3" style="margin: 10px auto;"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg" style="border-radius: 100%;width: 250px;height: 250px;"></div>
+                    <div class="col-md-6 col-lg-4 col-xl-3" style="margin: 10px auto;"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg" style="border-radius: 100%;width: 250px;height: 250px;"></div>
+                </div>
+            </div>
+        </section>
+        <section class="clean-block clean-info dark" style="background-color: rgb(255,255,255);">
+            <div class="container">
+                <div class="block-heading">
+                    <h1 style="color: rgb(239,197,133);font-family: Montserrat, sans-serif;">Escolha por <strong>TRATAMENTO</strong></h1>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-lg-4 col-xl-3" style="margin: 10px auto;"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg" style="border-radius: 100%;width: 250px;height: 250px;"></div>
+                    <div class="col-md-6 col-lg-4 col-xl-3" style="margin: 10px auto;"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg" style="border-radius: 100%;width: 250px;height: 250px;"></div>
+                    <div class="col-md-6 col-lg-4 col-xl-3" style="margin: 10px auto;"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg" style="border-radius: 100%;width: 250px;height: 250px;"></div>
+                </div>
+            </div>
+        </section>
+        <section class="clean-block features" style="background-color: rgb(244,244,244);">
+            <div class="container">
+                <div class="block-heading">
+                    <h1 style="color: #efc585;font-family: 'Dancing Script', cursive;">Sobre Nós</h1>
+                    <p style="font-family: 'Dancing Script', cursive;font-size: 24px;color: rgb(0,0,0);">A Corporalle é uma união do melhor em termos de estética e segurança em seus tratamentos.&nbsp;<br></p>
+                    <h2 style="color: #efc585;font-family: 'Dancing Script', cursive;margin-top: 32px;">Veja Fotos</h2>
+                </div>
+                <div class="row justify-content-center"><img style="background: url(https://images.pexels.com/photos/374071/pexels-photo-374071.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500) center / cover no-repeat;width: 70vw;height: 250px;padding-top: 0px;margin-top: -15px;"></div>
+            </div>
+        </section>
+        <section class="clean-block slider dark"></section>
+        <section class="clean-block about-us">
+            <div class="container">
+                <div class="block-heading">
+                    <h1 style="color: #efc585;font-family: 'Dancing Script', cursive;">Localização</h1>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-sm-6 col-lg-4" style="margin: auto;margin-top: 10px;">
+                        <h2 style="font-family: 'Dancing Script', cursive;text-align: center;">Spa</h2><iframe allowfullscreen="" frameborder="0" src="https://cdn.bootstrapstudio.io/placeholders/map.html" width="100%" height="400"></iframe>
+                        <h4 style="font-family: 'Dancing Script', cursive;text-align: center;">Endereço Spa:</h4>
+                        <h4 style="font-family: 'Dancing Script', cursive;text-align: center;">Telefone: (61) XXXXX-XXXX</h4>
+                        <h5 style="font-family: 'Dancing Script', cursive;text-align: center;">Horário de Funcionamento: 08:00 até 17:00</h5>
+                    </div>
+                    <div class="col-sm-6 col-lg-4" style="margin: auto;margin-top: 10px;">
+                        <h2 style="font-family: 'Dancing Script', cursive;text-align: center;">Clínica</h2><iframe allowfullscreen="" frameborder="0" src="https://cdn.bootstrapstudio.io/placeholders/map.html" width="100%" height="400"></iframe>
+                        <h4 style="font-family: 'Dancing Script', cursive;text-align: center;">Endereço Clínica:</h4>
+                        <h4 style="font-family: 'Dancing Script', cursive;text-align: center;">Telefone: (61) XXXXX-XXXX</h4>
+                        <h5 style="font-family: 'Dancing Script', cursive;text-align: center;">Horário de Funcionamento: 08:00 até 17:00</h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+   
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home - Corporalle</title>
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Hind+Siliguri">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/Footer-Basic.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Growing-Search-Bar-Animated-Text-Input.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/smoothproducts.css')}}">
 
 </head>
 <body>
@@ -19,6 +29,34 @@
 @yield('content')
 <!-- END CONTENT -->
 <!-- START FOOTER-->
-
+    <!-- Start: Footer Basic -->
+    <div class="footer-basic" style="background-color: #efc585;">
+        <footer>
+            <!-- Start: Social Icons -->
+            <div class="social" style="color: rgb(255,255,255);"><a href="#"><i class="icon ion-social-whatsapp"></i></a><a href="#"><i class="icon ion-email"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <!-- End: Social Icons -->
+            <!-- Start: Links -->
+            <ul class="list-inline" style="color: rgb(255,255,255);">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <!-- End: Links -->
+            <!-- Start: Copyright -->
+            <p class="copyright" style="color: rgb(255,255,255);">Corporalle - 2020</p>
+            <!-- End: Copyright -->
+            <!-- Start: Copyright -->
+            <p class="copyright" style="color: rgb(255,255,255);margin-top: 5px;">Desenvolvido por Artica Dev</p>
+            <!-- End: Copyright -->
+        </footer>
+    </div>
+    <!-- End: Footer Basic -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
 </body>
 </html>
